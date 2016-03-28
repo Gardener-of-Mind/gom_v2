c5259382a1d7b2766c673fb5a5a0eacc3e6cc715
satwik77 <satwik55@gmail.com>
2016-03-28 14:52:30 +0530

    update

--- GoM_django/Main/urls.py
+++ GoM_django/Main/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from Main import views
 
 urlpatterns = [
-    url(r'^index/$', views.index),
+    url(r'^$', views.index),
     url(r'^login/$', views.login),
+    url(r'^register/$', views.register),
 ]
